06a82bb51d696cca0d970c0a21aff6663f6af0a6
Listopaddon <67251998+Listopaddon@users.noreply.github.com>
2022-10-27 19:03:51 +0300

    not ready

---
+++ Lesson_7/ComparablePair.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    public class ComparablePair<T, U> : IComparable<ComparablePair<T, U>>   
+    {
+        T _Number1 { get; }
+        U _Number2 { get; }
+
+
+        public ComparablePair(T number1, U number2)
+        {
+            _Number1 = number1;
+            _Number2 = number2;
+        }
+
+        public int CompareTo(ComparablePair<T, U>? other)
+        {
+            if (_Number1 > other._Number1)
+            {
+                return 1;
+            }
+            else if (_Number1 < other._Number1)
+            {
+                return -1;
+            }
+            else if (_Number1 == other._Number1)
+            {
+                if (_Number2 > other._Number2)
+                {
+                    return 1;
+                }
+                else if (_Number2 < other._Number2)
+                {
+                    return -1;
+                }
+                else if (_Number2 == other._Number2)
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
